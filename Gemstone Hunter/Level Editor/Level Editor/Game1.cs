8353b720c3d089927764ccedaefe7eac512f9813
JasonRVCC <G00136378@W205-01.academic.raritanval.edu>
2015-12-03 15:39:03 -0500

    Chapter 8 (working)
    currently building game editor

---
+++ Gemstone Hunter/Level Editor/Level Editor/Game1.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Tile_Engine;
+
+namespace Level_Editor
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        IntPtr drawSurface;
+        System.Windows.Forms.Form parentForm;
+        System.Windows.Forms.PictureBox pictureBox;
+
+        System.Windows.Forms.Control gameForm;
+
+        public Game1(IntPtr drawSurface, System.Windows.Forms.Form parentForm, 
+            System.Windows.Forms.PictureBox pictureBox)
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            this.drawSurface = drawSurface;
+            this.parentForm = parentForm;
+            this.pictureBox = pictureBox;
+
+            graphics.PreparingDeviceSettings += 
+                new EventHandler<PreparingDeviceSettingsEventArgs>(graphics_PreparingDeviceSettings);
+
+            Mouse.WindowHandle = drawSurface;
+
+            gameForm = System.Windows.Forms.Control.FromHandle(this.Window.Handle);
+            gameForm.VisibleChanged += new EventHandler(gameForm_VisibleChanged);
+            gameForm.SizeChanged += new EventHandler(pictureBox_SizeChanged);
+        }
+
+        #region Event Handlers
+        void graphics_PreparingDeviceSettings(object sender, PreparingDeviceSettingsEventArgs e)
+        {
+            e.GraphicsDeviceInformation.PresentationParameters.DeviceWindowHandle = drawSurface;
+        }
+
+        private void gameForm_VisibleChanged(object sender, EventArgs e)
+        {
+            if (gameForm.Visible == true)
+            { gameForm.Visible = false; }
+        }
+
+        void pictureBox_SizeChanged(object sender, EventArgs e)
+        {
+            if (parentForm.WindowState != System.Windows.Forms.FormWindowState.Minimized)
+            {
+                graphics.PreferredBackBufferWidth = pictureBox.Width;
+                graphics.PreferredBackBufferHeight = pictureBox.Height;
+
+                Camera.ViewPortWidth = pictureBox.Width;
+                Camera.ViewPortHeight = pictureBox.Height;
+
+                graphics.ApplyChanges();
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Gemstone Hunter/Level Editor/Level Editor/MapEditor.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using System.IO;
+using Tile_Engine;
+
+namespace Level_Editor
+{
+    public partial class MapEditor : Form
+    {
+        public Game1 game;
+
+        public MapEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game.Exit();
+            Application.Exit();
+        }
+
+        private void LoadImageList()
+        {
+            string filepath = Application.StartupPath + @"\Content\Textures\PlatformTiles.png";
+            Bitmap tileSheet = new Bitmap(filepath);
+            int tilecount = 0;
+
+            for (int y = 0; y < tileSheet.Height / TileMap.TileHeight; y++)
+            {
+                for (int x = 0; x < tileSheet.Width / TileMap.TileWidth; x++)
+                {
+                    Bitmap newBitmap = tileSheet.Clone(new System.Drawing.Rectangle(x * TileMap.TileWidth,
+                        y * TileMap.TileHeight, TileMap.TileWidth, TileMap.TileHeight),
+                        System.Drawing.Imaging.PixelFormat.DontCare);
+                    imgListTiles.Images.Add(newBitmap);
+
+                    string itemName = "";
+                    if (tilecount == 0)
+                    { itemName = "Empty"; }
+                    if (tilecount == 1)
+                    { itemName = "White"; }
+                    listTiles.Items.Add(new ListViewItem(itemName, tilecount++));
+                }
+            }
+        }
+
+        private void MapEditor_Load(object sender, EventArgs e)
+        {
+            LoadImageList();
+            FixScrollBarScales();
+            FixScrollBarScales();
+
+            cboCodeValues.Items.Clear();
+            cboCodeValues.Items.Add("Gemstone");
+            cboCodeValues.Items.Add("Enemy");
+            cboCodeValues.Items.Add("Lethal");
+            cboCodeValues.Items.Add("EnemyBlocking");
+            cboCodeValues.Items.Add("Start");
+            cboCodeValues.Items.Add("Clear");
+            cboCodeValues.Items.Add("Custom");
+
+            for (int x = 0; x < 100; x++)
+            { cboMapNumber.Items.Add(x.ToString().PadLeft(3, '0')); }
+
+            cboMapNumber.SelectedIndex = 0;
+            TileMap.EditorMode = true;
+        }
+
+        private void FixScrollBarScales()
+        {
+            Camera.ViewPortWidth = pctSurface.Width;
+            Camera.ViewPortHeight = pctSurface.Height;
+
+            Camera.Move(Vector2.Zero);
+
+            vScrollBar1.Minimum = 0;
+            vScrollBar1.Maximum = Camera.WorldRectangle.Height - Camera.ViewPortHeight;
+
+            hScrollBar1.Minimum = 0;
+            hScrollBar1.Maximum = Camera.WorldRectangle.Width - Camera.ViewPortWidth;
+        }
+
+        private void MapEditor_Resize(object sender, EventArgs e)
+        {
+            FixScrollBarScales();
+        }
+
+        private void cboCodeValues_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtNewCode.Enabled = false;
+            switch (cboCodeValues.Items[cboCodeValues.SelectedIndex].ToString())
+            {
+                case "Gemstone":
+                    txtNewCode.Text = "GEM";
+                    break;
+                case "Enemy":
+                    txtNewCode.Text = "ENEMY";
+                    break;
+                case "Lethal":
+                    txtNewCode.Text = "DEAD";
+                    break;
+                case "EnemyBlocking":
+                    txtNewCode.Text = "BLOCK";
+                    break;
+                case "Start":
+                    txtNewCode.Text = "START";
+                    break;
+                case "Clear": 
+                    txtNewCode.Text = "";
+                    break;
+                case "Custom":
+                    txtNewCode.Text = "";
+                    txtNewCode.Enabled = true;
+                    break;
+            }
+        }
+    }
+}
